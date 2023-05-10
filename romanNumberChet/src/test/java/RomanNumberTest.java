29380749b8c707b05512dd9a693b468f0260eddc
11395100 <wijak.hongmala@krungsri.com>
2023-05-10 10:06:08 +0700

    new folder chet

---
+++ romanNumberChet/src/test/java/RomanNumberTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RomanNumberTest {
+
+    @Test
+    @DisplayName("1. Roman Numeral is I")
+    void getRomanValueEqualI() {
+
+        // Arrange
+
+        // Act
+
+        // Assert
+
+    }
+}
