bae63045149476582bf8fa0b44fb9cba1dad5e74
11395100 <wijak.hongmala@krungsri.com>
2023-05-10 10:23:39 +0700

    add roman number should be II

--- romanNumberChet/src/main/java/RomanNumber.java
+++ romanNumberChet/src/main/java/RomanNumber.java
@@ -1,8 +1,14 @@
 public class RomanNumber {
 
     public String covert(int input) {
-        String output = "I";
+        String output = "";
 
+        if (input == 1) {
+            output = "I";
+        }
+        if (input == 2) {
+            output = "II";
+        }
 
         return output;
     }
