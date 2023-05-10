8fbade63a244575827d70fc27bdac82b11eb5a23
Aungkana <aungkana.khamnuan@krungsri.com>
2023-05-10 10:45:38 +0700

    add test roman 5 10 100 500 1000 case

--- romanNumberAungkana/src/test/java/tdd/RomanNumberTest.java
+++ romanNumberAungkana/src/test/java/tdd/RomanNumberTest.java
@@ -21,6 +21,76 @@ public class RomanNumberTest {
     }
 
     @Test
+    public void romanNumber_of_5_should_be_V() {
+        // Arrange
+        int input = 5;
+        String expectedResult = "V";
+        RomanNumber romanNumber = new RomanNumber();
+
+        // Act
+        String actualResult = romanNumber.covert(input);
+
+        // Assert
+        assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void romanNumber_of_10_should_be_X() {
+        // Arrange
+        int input = 10;
+        String expectedResult = "X";
+        RomanNumber romanNumber = new RomanNumber();
+
+        // Act
+        String actualResult = romanNumber.covert(input);
+
+        // Assert
+        assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void romanNumber_of_100_should_be_C() {
+        // Arrange
+        int input = 100;
+        String expectedResult = "C";
+        RomanNumber romanNumber = new RomanNumber();
+
+        // Act
+        String actualResult = romanNumber.covert(input);
+
+        // Assert
+        assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void romanNumber_of_500_should_be_D() {
+        // Arrange
+        int input = 500;
+        String expectedResult = "D";
+        RomanNumber romanNumber = new RomanNumber();
+
+        // Act
+        String actualResult = romanNumber.covert(input);
+
+        // Assert
+        assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void romanNumber_of_1000_should_be_M() {
+        // Arrange
+        int input = 1000;
+        String expectedResult = "M";
+        RomanNumber romanNumber = new RomanNumber();
+
+        // Act
+        String actualResult = romanNumber.covert(input);
+
+        // Assert
+        assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
     public void romanNumber_of_2_should_be_II() {
         // Arrange
         int input = 2;
