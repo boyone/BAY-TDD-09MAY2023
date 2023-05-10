435afaeaadf6aa8676b22812ca037264d53bdaf8
Aungkana <aungkana.khamnuan@krungsri.com>
2023-05-10 10:37:05 +0700

    Reformat code

--- romanNumberAungkana/src/main/java/tdd/RomanNumber.java
+++ romanNumberAungkana/src/main/java/tdd/RomanNumber.java
@@ -2,15 +2,15 @@ package tdd;
 
 public class RomanNumber {
 
-    String covert(int input){
-        if(input == 1){
+    String covert(int input) {
+        if (input == 1) {
             return "I";
-        }else if(input == 2){
+        } else if (input == 2) {
             return "II";
-        }else if(input == 3){
+        } else if (input == 3) {
             return "III";
         }
-       return "";
+        return "";
     }
-    
+
 }
