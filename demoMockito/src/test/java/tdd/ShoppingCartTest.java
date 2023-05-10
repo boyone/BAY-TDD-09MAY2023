87a28e1dee0c6c66de3b920e0b1d74635f3e7900
Kanavood <donot_knowme@hotmail.com>
2023-05-10 16:55:03 +0700

    verify mock

--- demoMockito/src/test/java/tdd/ShoppingCartTest.java
+++ demoMockito/src/test/java/tdd/ShoppingCartTest.java
@@ -5,6 +5,8 @@ import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class ShoppingCartTest {
@@ -24,5 +26,22 @@ public class ShoppingCartTest {
         // Assert
         assertEquals(expectedResult, actualResult);
 
+    }
+
+    @Test
+    public void verifyTest() {
+        // Arrange
+        ProductDetail expectedResult = new ProductDetail(1, "adidas", 500, 1000);
+
+        Catalog catalog = Mockito.mock(Catalog.class);
+        when(catalog.find(1)).thenReturn(new Product("adidas",500));
+        ShoppingCart shopping = new ShoppingCart(catalog);
+
+        // Act
+        ProductDetail actualResult = shopping.detail(1, 2);
+
+        // Assert
+        verify(catalog).find(1);
+        verify(catalog, times(1)).find(1);
     }
 }
