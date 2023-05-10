7b857e58254859687ded7129a56cf0e0fc210d48
Chanut Wongtripipat <11414968@krungsri.com>
2023-05-10 15:44:21 +0700

    initial first test case

--- demoMockito/src/test/java/tdd/ShoppingCartTest.java
+++ demoMockito/src/test/java/tdd/ShoppingCartTest.java
@@ -2,10 +2,20 @@ package tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ShoppingCartTest {
 
     @Test
     public void stubTest() {
+        // Arrange
+        ProductDetail expectedResult = new ProductDetail();
+        ShoppingCart shopping = new ShoppingCart();
 
+        // Act
+        ProductDetail actualResult = shopping.detail(1, 2);
+
+        // Assert
+        assertEquals(expectedResult, actualResult);
     }
 }
