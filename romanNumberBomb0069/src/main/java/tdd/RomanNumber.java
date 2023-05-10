7c6d6bbb3d14367010d1cf1b5526b318f0a76cb7
karan sivarat <bomb0069@gmail.com>
2023-05-10 11:36:40 +0700

    [Added]: specific char for roman

--- romanNumberBomb0069/src/main/java/tdd/RomanNumber.java
+++ romanNumberBomb0069/src/main/java/tdd/RomanNumber.java
@@ -1,7 +1,19 @@
 package tdd;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanNumber {
+    private Map<Integer, String> romanNumberMapping = new HashMap<Integer, String>() {{
+        put(1, "I");
+        put(5, "V");
+        put(10, "X");
+        put(50, "L");
+        put(100, "C");
+        put(500, "D");
+        put(1000, "M");
+    }};
     public String covert(int input) {
-        return "I";
+        return romanNumberMapping.get(input);
     }
 }
--- romanNumberBomb0069/src/test/java/tdd/RomanNumberTest.java
+++ romanNumberBomb0069/src/test/java/tdd/RomanNumberTest.java
@@ -20,4 +20,57 @@ public class RomanNumberTest {
         assertEquals(expectedResult, actualResult);
     }
 
+    @Test
+    public void romanNumber_of_5_should_be_V() {
+        // Arrange
+        int input = 5;
+        String expectedResult ="V";
+        RomanNumber romanNumber = new RomanNumber();
+
+        // Act
+        String actualResult = romanNumber.covert(input);
+
+        // Assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void romanNumber_of_10_should_be_X() {
+        // Arrange
+        int input = 10;
+        String expectedResult ="X";
+        RomanNumber romanNumber = new RomanNumber();
+
+        // Act
+        String actualResult = romanNumber.covert(input);
+
+        // Assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void romanNumber_of_50_should_be_L() {
+        // Arrange
+        int input = 50;
+        String expectedResult ="L";
+        RomanNumber romanNumber = new RomanNumber();
+
+        // Act
+        String actualResult = romanNumber.covert(input);
+
+        // Assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void romanNumber_of_100_500_1000_should_be_C_D_M() {
+        // Arrange
+        RomanNumber romanNumber = new RomanNumber();
+
+        // Act
+        // Assert
+        assertEquals("C", romanNumber.covert(100));
+        assertEquals("D", romanNumber.covert(500));
+        assertEquals("M", romanNumber.covert(1000));
+    }
 }
