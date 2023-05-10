b0c67f9eca17c9c5eb86cbf0b45870daa3596f40
Nattavadh Kanthamarn <Nattavadh.Kanthamarn@krungsri.com>
2023-05-10 15:04:22 +0700

    add testcase with more complexity and refactoring code

--- romanNumberNattavadh/src/main/java/tdd/RomanNumber.java
+++ romanNumberNattavadh/src/main/java/tdd/RomanNumber.java
@@ -17,25 +17,97 @@ public class RomanNumber {
 
   private String calculate(int input) {
     StringBuilder result = new StringBuilder();
-    if (1 <= input && 3 >= input) {
-      for (int i = 1; i <= input; i++) {
-        result.append(ONE);
-      }
-    } else if(4 == input){
-      result = new StringBuilder().append(ONE).append(FIVE);
-    }else if (5 == input) {
-      result = new StringBuilder(FIVE);
-    } else if (10 == input) {
-      result = new StringBuilder(TEN);
-    } else if (50 == input) {
-      result = new StringBuilder(FIFTY);
-    } else if (100 == input) {
-      result = new StringBuilder(HUNDRED);
-    } else if (500 == input) {
-      result = new StringBuilder(FIVE_HUNDRED);
-    } else if (1000 == input) {
-      result = new StringBuilder(THOUSAND);
+
+    int thousandNumber = input / 1000;
+    int remaining = input % 1000;
+    String mReferThousand = "";
+    if (thousandNumber > 0) {
+      mReferThousand = forLoopSymbol(thousandNumber * 1000, 1000, THOUSAND, new StringBuilder()).toString();
     }
-    return result.toString();
+
+    int fiveHundredNumber = remaining / 500;
+    remaining = remaining % 500;
+    String dReferFiveHundred = "";
+    if (fiveHundredNumber > 0) {
+      dReferFiveHundred = FIVE_HUNDRED;
+    }
+    int hundredNumber = remaining / 100;
+    remaining = remaining % 100;
+    String cReferHundred = "";
+    if (hundredNumber > 0) {
+      cReferHundred = forLoopSymbol(hundredNumber * 100, 100, HUNDRED, new StringBuilder()).toString();
+    }
+    if (fiveHundredNumber == 1 && hundredNumber == 4) {
+      dReferFiveHundred = "";
+      cReferHundred = HUNDRED + THOUSAND;
+    }
+    if (fiveHundredNumber == 0 && hundredNumber == 4) {
+      dReferFiveHundred = "";
+      cReferHundred = HUNDRED + FIVE_HUNDRED;
+    }
+
+    int fiftyNumber = remaining / 50;
+    remaining = remaining % 50;
+    String lReferFifty = "";
+    if (fiftyNumber > 0) {
+      lReferFifty = FIFTY;
+    }
+
+    int tenNumber = remaining / 10;
+    remaining = remaining % 10;
+    String xReferTen = "";
+    if (tenNumber > 0) {
+      xReferTen = forLoopSymbol(tenNumber * 10, 10, TEN, new StringBuilder()).toString();
+    }
+    if (fiftyNumber == 1 && tenNumber == 4) {
+      lReferFifty = "";
+      xReferTen = TEN + HUNDRED;
+    } else if (fiftyNumber == 0 && tenNumber == 4) {
+      lReferFifty = "";
+      xReferTen = TEN + FIFTY;
+    }
+
+    int fiveNumber = remaining / 5;
+    remaining = remaining % 5;
+    String vReferFive = "";
+    if (fiveNumber > 0) {
+      vReferFive = FIVE;
+    }
+
+    String oneNumber = "";
+    if (remaining > 0) {
+      oneNumber = forLoopSymbol(remaining, 1, ONE, new StringBuilder()).toString();
+    }
+    if (fiveNumber == 1 && remaining == 4) {
+      vReferFive = "";
+      oneNumber = ONE + TEN;
+    }
+    if (fiveNumber == 0 && remaining == 4) {
+      vReferFive = "";
+      oneNumber = ONE + FIVE;
+    }
+
+    return result
+        .append(mReferThousand)
+        .append(dReferFiveHundred)
+        .append(cReferHundred)
+        .append(lReferFifty)
+        .append(xReferTen)
+        .append(vReferFive)
+        .append(oneNumber)
+        .toString();
+  }
+
+  private StringBuilder forLoopSymbol(int input, int increase, String symbol,
+      StringBuilder result) {
+    if (input == 0) {
+      return new StringBuilder();
+    }
+    int i = increase;
+    do {
+      result.append(symbol);
+      i = i + increase;
+    } while (i <= input);
+    return result;
   }
 }
--- romanNumberNattavadh/src/test/java/tdd/RomanNumberTest.java
+++ romanNumberNattavadh/src/test/java/tdd/RomanNumberTest.java
@@ -1,176 +1,113 @@
 package tdd;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-public class RomanNumberTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RomanNumberTest {
 
   @Test
-  @DisplayName("case 1: input 1 should be return \"I\"")
-  public void test_number1_shouldReturnI() {
+  @DisplayName("case 1: Expect with one digit symbol \"I\",\"V\",\"X\",\"L\",\"C\",\"D\",\"M\"")
+  void assertOneDigit() {
     // Arrange
-    int input = 1;
-    String expectedResult = "I";
     RomanNumber romanNumber = new RomanNumber();
-
     // Act
-    String actualResult = romanNumber.convert(input);
 
     // Assert
-    assertEquals(expectedResult, actualResult);
+    assertAll("Expect with one digit symbol",
+        () -> assertEquals("I", romanNumber.convert(1), "1 == I"),
+        () -> assertEquals("V", romanNumber.convert(5), "5 == V"),
+        () -> assertEquals("X", romanNumber.convert(10), "10 == X"),
+        () -> assertEquals("L", romanNumber.convert(50), "50 == L"),
+        () -> assertEquals("C", romanNumber.convert(100), "100 == C"),
+        () -> assertEquals("D", romanNumber.convert(500), "500 == D"),
+        () -> assertEquals("M", romanNumber.convert(1000), "1000 == M"));
   }
 
   @Test
-  @DisplayName("case 2: input 5 should be return \"V\"")
-  public void test_number5_shouldReturnV() {
+  @DisplayName("case 2: Expect with duplicated digit symbol \"II\",\"III\",\"XX\",\"XXX\",\"CC\",\"CCC\",\"MM\",\"MMM\"")
+  void assertDuplicateDigit() {
     // Arrange
-    int input = 5;
-    String expectedResult = "V";
     RomanNumber romanNumber = new RomanNumber();
 
     // Act
-    String actualResult = romanNumber.convert(input);
 
     // Assert
-    assertEquals(expectedResult, actualResult);
+    assertAll("Expect with duplicated digit symbol",
+        () -> assertEquals("II", romanNumber.convert(2)),
+        () -> assertEquals("III", romanNumber.convert(3)),
+        () -> assertEquals("XX", romanNumber.convert(20)),
+        () -> assertEquals("XXX", romanNumber.convert(30)),
+        () -> assertEquals("CC", romanNumber.convert(200)),
+        () -> assertEquals("CCC", romanNumber.convert(300)),
+        () -> assertEquals("MM", romanNumber.convert(2000)),
+        () -> assertEquals("MMM", romanNumber.convert(3000)));
   }
 
   @Test
-  @DisplayName("case 3: input 10 should be return \"X\"")
-  public void test_number10_shouldReturnX() {
+  @DisplayName("case 3: Expect Combination with prefix symbol")
+  void assertPrefixCombinationSymbol() {
     // Arrange
-    int input = 10;
-    String expectedResult = "X";
     RomanNumber romanNumber = new RomanNumber();
 
     // Act
-    String actualResult = romanNumber.convert(input);
 
     // Assert
-    assertEquals(expectedResult, actualResult);
+    assertAll("Expect Combination with prefix symbol",
+        () -> assertEquals("IV", romanNumber.convert(4)),
+        () -> assertEquals("IX", romanNumber.convert(9)),
+        () -> assertEquals("XL", romanNumber.convert(40)),
+        () -> assertEquals("XC", romanNumber.convert(90)),
+        () -> assertEquals("CD", romanNumber.convert(400)),
+        () -> assertEquals("CM", romanNumber.convert(900)));
   }
 
   @Test
-  @DisplayName("case 4: input 50 should be return \"L\"")
-  public void test_number50_shouldReturnL() {
+  @DisplayName("case 4: Expect Combination with suffix symbol")
+  void assertSuffixCombinationSymbol() {
     // Arrange
-    int input = 10;
-    String expectedResult = "X";
     RomanNumber romanNumber = new RomanNumber();
 
     // Act
-    String actualResult = romanNumber.convert(input);
 
     // Assert
-    assertEquals(expectedResult, actualResult);
+    assertAll("Expect Combination with suffix symbol",
+        () -> assertEquals("VI", romanNumber.convert(6)),
+        () -> assertEquals("VII", romanNumber.convert(7)),
+        () -> assertEquals("VIII", romanNumber.convert(8)),
+
+        () -> assertEquals("XI", romanNumber.convert(11)),
+        () -> assertEquals("XII", romanNumber.convert(12)),
+        () -> assertEquals("XIII", romanNumber.convert(13)),
+
+        () -> assertEquals("LX", romanNumber.convert(60)),
+        () -> assertEquals("LXX", romanNumber.convert(70)),
+        () -> assertEquals("LXXX", romanNumber.convert(80)),
+
+        () -> assertEquals("CX", romanNumber.convert(110)),
+        () -> assertEquals("CXX", romanNumber.convert(120)),
+        () -> assertEquals("CXXX", romanNumber.convert(130)),
+
+        () -> assertEquals("DC", romanNumber.convert(600)),
+        () -> assertEquals("DCC", romanNumber.convert(700)),
+        () -> assertEquals("DCCC", romanNumber.convert(800)));
   }
 
   @Test
-  @DisplayName("case 5: input 100 should be return \"C\"")
-  public void test_number100_shouldReturnC() {
+  @DisplayName("case 5: Number with complexity")
+  void assertNumberWithComplexity() {
     // Arrange
-    int input = 100;
-    String expectedResult = "C";
     RomanNumber romanNumber = new RomanNumber();
 
     // Act
-    String actualResult = romanNumber.convert(input);
 
     // Assert
-    assertEquals(expectedResult, actualResult);
+    assertAll("Number with complexity",
+        () -> assertEquals("MCCXXXIV", romanNumber.convert(1234)),
+        () -> assertEquals("CMXCIX", romanNumber.convert(999)),
+        () -> assertEquals("CDXLIV", romanNumber.convert(444)));
   }
-
-  @Test
-  @DisplayName("case 6: input 500 should be return \"D\"")
-  public void test_number500_shouldReturnD() {
-    // Arrange
-    int input = 500;
-    String expectedResult = "D";
-    RomanNumber romanNumber = new RomanNumber();
-
-    // Act
-    String actualResult = romanNumber.convert(input);
-
-    // Assert
-    assertEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  @DisplayName("case 7: input 1000 should be return \"M\"")
-  public void test_number1000_shouldReturnM() {
-    // Arrange
-    int input = 1000;
-    String expectedResult = "M";
-    RomanNumber romanNumber = new RomanNumber();
-
-    // Act
-    String actualResult = romanNumber.convert(input);
-
-    // Assert
-    assertEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  @DisplayName("case 8: input 2 should be return \"II\"")
-  public void test_number2_shouldReturnII() {
-    // Arrange
-    int input = 2;
-    String expectedResult = "II";
-    RomanNumber romanNumber = new RomanNumber();
-
-    // Act
-    String actualResult = romanNumber.convert(input);
-
-    // Assert
-    assertEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  @DisplayName("case 9: input 3 should be return \"III\"")
-  public void test_number3_shouldReturnIII() {
-    // Arrange
-    int input = 3;
-    String expectedResult = "III";
-    RomanNumber romanNumber = new RomanNumber();
-
-    // Act
-    String actualResult = romanNumber.convert(input);
-
-    // Assert
-    assertEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  @DisplayName("case 10: input 4 should not equal \"IIII\"")
-  public void test_number4_shouldNotEqualsIIII() {
-    // Arrange
-    int input = 4;
-    String expectedResult = "IIII";
-    RomanNumber romanNumber = new RomanNumber();
-
-    // Act
-    String actualResult = romanNumber.convert(input);
-
-    // Assert
-    assertNotEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  @DisplayName("case 10: input 4 should not equal \"IV\"")
-  public void test_number4_shouldReturnIV() {
-    // Arrange
-    int input = 4;
-    String expectedResult = "IV";
-    RomanNumber romanNumber = new RomanNumber();
-
-    // Act
-    String actualResult = romanNumber.convert(input);
-
-    // Assert
-    assertEquals(expectedResult, actualResult);
-  }
-
 }
